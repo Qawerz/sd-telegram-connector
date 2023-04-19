06da135b1ace6938123682826811ed23768dd409
Qawerz <39441645+Qawerz@users.noreply.github.com>
2023-04-19 14:10:40 +0300

    Add files via upload

--- WebApp/script.js
+++ WebApp/script.js
@@ -1,12 +1,18 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-let btn = document.getElementById("btn");
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
 
 let p_prompt = document.getElementById("p_prompt")
 let n_prompt = document.getElementById("n_prompt")
 
-btn.addEventListener("click",function(){
-	tg.sendData({"prompt":p_prompt.value, }); 
-    tg.close()
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.show();
+    tg.sendData({"prompt":p_prompt.value, });
 });
+
