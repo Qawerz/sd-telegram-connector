0351b2d78aeceb23933277381742ceefcdc03441
Qawerz <39441645+Qawerz@users.noreply.github.com>
2023-04-19 13:49:44 +0300

    Add files via upload

--- WebApp/script.js
+++ WebApp/script.js
@@ -1,19 +1,18 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
+tg.MainButton.setText("Отправить запрос");
+tg.MainButton.show();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#FF00FF";
+// tg.MainButton.textColor = "#FFFFFF";
+// tg.MainButton.color = "#FF00FF";
 
 let btn = document.getElementById("btn");
 
-btn.addEventListener("click", function(){
-    tg.MainButton.setText("Сообщение отправлено!");
-    tg.MainButton.show();
-    tg.sendData("sendTestMessage");
-});
+let p_prompt = document.getElementById("p_prompt")
+let n_prompt = document.getElementById("n_prompt")
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-	tg.sendData("some string that we need to send"); 
+	tg.sendData({p_prompt, n_prompt}); 
 	//при клике на основную кнопку отправляем данные в строковом виде
 });
