214f8e3c36fdf3224168660f8b685e38c4cb9b2e
Qawerz <39441645+Qawerz@users.noreply.github.com>
2023-04-19 14:01:42 +0300

    Add files via upload

--- WebApp/script.js
+++ WebApp/script.js
@@ -9,5 +9,5 @@ let p_prompt = document.getElementById("p_prompt")
 let n_prompt = document.getElementById("n_prompt")
 
 btn.addEventListener("click",function(){
-	tg.sendData({p_prompt, n_prompt}); 
+	tg.sendData({"prompt":p_prompt.value, }); 
 });
