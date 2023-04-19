f8e808fe1ffd6f01256e395de6fe7a40221a3a02
Qawerz <39441645+Qawerz@users.noreply.github.com>
2023-04-19 15:31:20 +0300

    Add files via upload

--- WebApp/script.js
+++ WebApp/script.js
@@ -2,16 +2,25 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#FF00FF";
-
 let p_prompt = document.getElementById("p_prompt")
 let n_prompt = document.getElementById("n_prompt")
+let width = document.getElementById("n_width")
+let height = document.getElementById("n_height")
+let seed = document.getElementById("seed")
+let steps = document.getElementById("n_steps")
 
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(){
-    tg.sendData(JSON.stringify({"prompt": p_prompt.value}));
+    tg.sendData(JSON.stringify(
+        {
+            "prompt": p_prompt.value,
+            "negative_prompt": n_prompt.value,
+            "steps": steps.value,
+            "width": width.value,
+            "height": height.value,
+            "seed": seed.value
+        }));
     tg.close();
 });
 
